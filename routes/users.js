511c0ddc921b90a051cdb0fea2a3d5c907ba9fc4
Tdoval <thiagodovalaraujo@gmail.com>
2021-07-28 21:34:42 -0300

    Hash password & Register Route

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
@@ -14,13 +15,39 @@ router.post('/', [
     check('email', 'Please include a valid email').isEmail(),
     check('password', 'Please enter a password with 6 or more characters').isLength({ min:6 })
 ],
- (req, res) => {
+ async (req, res) => {
     const errors = validationResult(req);
     if(!errors.isEmpty()) {
         return res.status(400).json({ errors: error.array() });
     }
 
-    res.send('passed');
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findOne( {email:email } );
+
+        if(user) {
+            return res.status(400).json({ msg: 'User already exists' })
+        }
+
+        user = new User( {
+            name:name,
+            email:email,
+            password:password
+        });
+
+        const salt = await bcrypt.genSalt(10);
+
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        res.send('User saved');
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 module.exports = router;
