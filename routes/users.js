27666ca58ffba4be21bd57e16b91a0c4ebfbc4ed
Tdoval <thiagodovalaraujo@gmail.com>
2021-07-28 21:44:19 -0300

    Create user and respond with Json Web Token

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check');
 
 const User = require('../models/User');
@@ -42,8 +44,18 @@ router.post('/', [
 
         await user.save();
 
-        res.send('User saved');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        }
 
+        jwt.sign(payload, config.get('jwtSecret'), {
+            expiresIn: 3600
+        }, (err, token) => {
+            if(err) throw err;
+            res.json({ token });
+        });
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
